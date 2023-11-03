8b7a3d73741b1860bef4c968bfa2dd68da1e93dc
Muhammad Atif <90444618+atifkhan77@users.noreply.github.com>
2023-11-03 22:31:39 +0500

    Time: 296 ms (5.61%), Space: 143 MB (27.73%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.dart
@@ -0,0 +1,19 @@
+class Solution {
+  void merge(List<int> nums1, int m, List<int> nums2, int n) {
+    
+    int i=m-1 , j=n-1 ;
+        while(i>=0 && j>=0 ){
+            if(nums1[i]>=nums2[j]){
+                nums1[i+j+1]=nums1[i] ;
+                i-- ;
+                }else{
+                    nums1[i+j+1]=nums2[j];
+                    j-- ;
+                }
+        }
+        while(j>=0){
+                    nums1[j]=nums2[j];
+                    j-- ;
+                }
+  }
+}
